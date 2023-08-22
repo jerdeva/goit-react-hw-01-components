5bdd8d3df861a89750af1af87a008a7b23ccf837
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-08-22 14:26:27 +0300

    clear

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { UserForm } from "./Profile/UserForm";
-import user from "../user.json"
-import data from "../data.json"
-import friend from '../friends.json'
-import transaction from '../transactions.json'
+import user from "../Data/user.json"
+import data from "../Data/data.json"
+import friend from '../Data/friends.json'
+import transaction from '../Data/transactions.json'
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./Friends/FriendList";
 import { TransactionHistory } from "./Transactions/TransactionHistory";
